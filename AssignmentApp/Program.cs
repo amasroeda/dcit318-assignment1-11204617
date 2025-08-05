913d0cfec07c0d4f87c268c993c48f97c6022828
amasroeda <amasroeda@gmail.com>
2025-08-05 11:52:38 +0000

    Completed Task 2: Ticket Price Calculator

--- AssignmentApp/Program.cs
+++ AssignmentApp/Program.cs
@@ -4,29 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter a numerical grade (0 - 100): ");
+        Console.Write("Enter your age: ");
         string? input = Console.ReadLine();
 
-        if (int.TryParse(input, out int grade))
+        if (int.TryParse(input, out int age))
         {
-            string letter;
-
-            if (grade >= 90)
-                letter = "A";
-            else if (grade >= 80)
-                letter = "B";
-            else if (grade >= 70)
-                letter = "C";
-            else if (grade >= 60)
-                letter = "D";
-            else
-                letter = "F";
-
-            Console.WriteLine("Letter Grade: " + letter);
+            int price = (age <= 12 || age >= 65) ? 7 : 10;
+            Console.WriteLine("Ticket Price: GHC" + price);
         }
         else
         {
-            Console.WriteLine("Invalid input. Please enter a number between 0 and 100.");
+            Console.WriteLine("Invalid input. Please enter a valid age.");
         }
     }
 }
